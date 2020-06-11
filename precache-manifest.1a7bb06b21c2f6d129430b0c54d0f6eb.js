efd2fac6273223a9e935880d6fcc3b34d3d0bf3a
Ernesto B <ejbb3t@gmail.com>
2020-06-11 01:43:05 -0400

    Updates

---
+++ precache-manifest.1a7bb06b21c2f6d129430b0c54d0f6eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa07e21aefcf8d172830e4b92c0592e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c738fe3f2e88636852f",
+    "url": "/static/css/main.ddfa0cad.chunk.css"
+  },
+  {
+    "revision": "3f54c620a27b6700d8fd",
+    "url": "/static/js/2.e5a093f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e5a093f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c738fe3f2e88636852f",
+    "url": "/static/js/main.4284d58b.chunk.js"
+  },
+  {
+    "revision": "d5b38e6023c3f548d85d",
+    "url": "/static/js/runtime-main.058325c3.js"
+  }
+]);
